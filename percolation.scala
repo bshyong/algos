1501e0799b8b4a88609878c88f1412ca78603b16
Ben Shyong <likeachild@gmail.com>
2013-09-08 03:16:55 -0400

    initial commit

---
+++ percolation.scala
@@ -0,0 +1,11 @@
+class Percolation {
+   // Percolation(int N)              // create N-by-N grid, with all sites blocked
+   // void open(int i, int j)         // open site (row i, column j) if it is not already
+   // boolean isOpen(int i, int j)    // is site (row i, column j) open?
+   // boolean isFull(int i, int j)    // is site (row i, column j) full?
+
+   // does the system percolate?
+   def percolates() : Boolean = {
+    return true;
+   }
+}
