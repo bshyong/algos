7c7f0ed1833054f8add5c9b6c5375f77fc970bf4
Benjamin Shyong <ben.shyong@gmail.com>
2014-07-01 16:37:26 -0700

    initializer

--- union_find/WeightedQuickUnionFind.rb
+++ union_find/WeightedQuickUnionFind.rb
@@ -1,6 +1,7 @@
 class WeightedQuickUnionFind
 
   attr_reader :sizes, :roots, :count
+
   # Sizes holds the number of objects
   # in the subtree at each root
   # sizes[i] = objects in subtree with root i
@@ -8,9 +9,9 @@ class WeightedQuickUnionFind
 
   def initialize(n)
     # TODO: catch if count is not an integer
-    count = n
-    sizes = Array.new(count)
-    roots = Array.new(count)
+    @count = n
+    @sizes = Array.new(count)
+    @roots = Array.new(count)
 
     (0..n).each do |x|
       # set roots to itself since all nodes are orphans
