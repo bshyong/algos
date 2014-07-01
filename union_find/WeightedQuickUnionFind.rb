d9e8edd6ee34b55f611bfdb49cba853109696819
Benjamin Shyong <ben.shyong@gmail.com>
2014-07-01 16:30:58 -0700

    quick union initialize

---
+++ union_find/WeightedQuickUnionFind.rb
@@ -0,0 +1,23 @@
+class WeightedQuickUnionFind
+
+  attr_reader :sizes, :roots, :count
+  # Sizes holds the number of objects
+  # in the subtree at each root
+  # sizes[i] = objects in subtree with root i
+  # Roots holds the parent of each object
+
+  def initialize(n)
+    # TODO: catch if count is not an integer
+    count = n
+    sizes = Array.new(count)
+    roots = Array.new(count)
+
+    (0..n).each do |x|
+      # set roots to itself since all nodes are orphans
+      roots[x] = x
+      sizes[x] = 1
+    end
+  end
+
+end
+
--- union_find/union_find.md
+++ union_find/union_find.md
@@ -5,6 +5,9 @@ Connect two objects, and find whether there is a path containing the two objects
 
 This is useful in many applications where it is necessary to find out whether two items are connected through a path. With a proper algorithm, even for a set of a billion objects (1 * 10^9), the worst case time can be reduced to something on the order of N*logN time.
 
+The implementation of this algorithm is a weighted quick union (weighted by number of objects).
+
+
 
 
 
